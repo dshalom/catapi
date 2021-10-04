be2e11a40c1987b5ed365727ec78243f7ad4743c
David Shalom <david.shalom@sky.uk>
2021-10-04 15:48:35 +0100

    running a stand alone instance

--- app/src/androidTest/java/com/example/catapi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/catapi/ExampleInstrumentedTest.kt
@@ -10,7 +10,10 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.catapi.utils.AssetsFileSource
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.matching.RequestPattern
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -21,52 +24,52 @@ import java.net.URL
 @RunWith(AndroidJUnit4::class)
 class CatImageLoadingTest {
 
-    private lateinit var wireMockServer: WireMockServer
+    // private lateinit var wireMockServer: WireMockServer
     lateinit var scenario: ActivityScenario<MainActivity>
+
+    lateinit var wireMock: WireMock
 
     @Before
     fun setUp() {
 
-        wireMockServer = WireMockServer(
-            8080,
+        wireMock = WireMock("localhost", 8080)
+        wireMock.resetMappings()
 
-            AssetsFileSource(subDirectoryName = "wiremock-checks"), false
-        )
-
-        wireMockServer.start()
-        Log.e("dsds", "is running on post" + wireMockServer.port())
     }
 
     @After
     fun tearDown() {
-        wireMockServer.stop()
+   //     wireMock.shutdown()
         scenario.close()
     }
 
     @Test
     fun loadCatImage1() {
 
-        stubFor(
-            get(urlPathMatching("/v1/images/search"))
-                .willReturn(
-                    ok()
-                        .withHeader("Content-Type", "text/json")
-                        .withBody(
-                            "[\n" +
-                                    "{\n" +
-                                    "\"breeds\": [],\n" +
-                                    "\"id\": \"bfc\",\n" +
-                                    "\"url\": \"https://cdn2.thecatapi.com.bingo/images/bfc.jpg\",\n" +
-                                    "\"width\": 501,\n" +
-                                    "\"height\": 333666\n" +
-                                    "}\n" +
-                                    "]"
-                        )
-                )
-        );
+        wireMock.register(
+            stubFor(
+                get(urlPathMatching("/v1/images/search"))
+                    .willReturn(
+                        ok()
+                            .withHeader("Content-Type", "text/json")
+                            .withBody(
+                                "[\n" +
+                                        "{\n" +
+                                        "\"breeds\": [],\n" +
+                                        "\"id\": \"bfc\",\n" +
+                                        "\"url\": \"https://cdn2.thecatapi.com.bingo/images/bfc.jpg\",\n" +
+                                        "\"width\": 501,\n" +
+                                        "\"height\": 333666\n" +
+                                        "}\n" +
+                                        "]"
+                            )
+                    )
+            )
+        )
 
-        val mappings = URL("http://localhost:${wireMockServer.port()}/__admin/mappings").readText()
-        Log.d("dsds", "mappings from wiremock: $mappings")
+
+//        val mappings = URL("http://localhost:${wireMockServer.port()}/__admin/mappings").readText()
+//        Log.d("dsds", "mappings from wiremock: $mappings")
 
         scenario = launchActivity()
 
@@ -74,13 +77,13 @@ class CatImageLoadingTest {
             .check(matches(isDisplayed()));
     }
 
-    @Test
-    fun loadCatImage2() {
-        val mappings = URL("http://localhost:${wireMockServer.port()}/__admin/mappings").readText()
-        Log.d("dsds", "mappings from wiremock: $mappings")
-
-        scenario = launchActivity()
-        onView(withId(R.id.iv))
-            .check(matches(isDisplayed()));
-    }
+//    @Test
+//    fun loadCatImage2() {
+//        val mappings = URL("http://localhost:${wireMockServer.port()}/__admin/mappings").readText()
+//        Log.d("dsds", "mappings from wiremock: $mappings")
+//
+//        scenario = launchActivity()
+//        onView(withId(R.id.iv))
+//            .check(matches(isDisplayed()));
+//    }
 }
--- app/src/main/java/com/example/catapi/MainActivity.kt
+++ app/src/main/java/com/example/catapi/MainActivity.kt
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
     @Inject
     lateinit var catService: CatService
